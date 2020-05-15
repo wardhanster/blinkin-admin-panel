6aa39e742698a3fb3d856235dbb26249d75cf0ca
ranjith <ranjith.v@blinkin.io>
2020-05-15 19:28:33 +0530

    updated build of logs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Users from "./Users";
 import Dashboard from "./Dashboard";
 import AddUsers from "./AddUsers";
+import Logs from "./Logs";
 
-export { Users, Dashboard, AddUsers };
+export { Users, Dashboard, AddUsers, Logs };
