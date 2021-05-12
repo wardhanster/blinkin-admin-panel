823de86ab3ffde306668cde417ec35f0cf4ee39b
manish-blinkin <manish.kumar@blinkin.io>
2021-05-12 12:48:12 +0530

    PREP-625: public link removed from preview

--- src/MediaLibrary/lib/FileUpload/FileUploadList.js
+++ src/MediaLibrary/lib/FileUpload/FileUploadList.js
@@ -87,12 +87,6 @@ export default function FileUploadList(props) {
       setSelectedTags([]);
     }
 
-    if (files[index].is_global) {
-      setMakePublic(files[index].is_global || false);
-    } else {
-      setMakePublic(false);
-    }
-
   };
 
   let handleDelete = (index, e) => {
@@ -273,17 +267,6 @@ export default function FileUploadList(props) {
                       rows="3"
                     ></textarea>
                   </FormGroup>
-                </Col>
-                <Col className="md-12 ml-3">
-                <FormGroup row>
-                  <FormGroup check>
-                    <Label check>
-                      <Input checked={makePublic}
-                      onChange={(e) => setMakePublic(e.target.checked)} type="checkbox" id="makePublic" />{' '}
-                      {window.strings.ML_makePublic || "Allow others to see ?"}
-                    </Label>
-                  </FormGroup>
-                </FormGroup>
                 </Col>
                 <Col md={12}>
                   <Row>
